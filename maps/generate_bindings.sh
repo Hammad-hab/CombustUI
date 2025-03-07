52e91299c396a28f16083a835b9a835b7bbccf2a
CW team <hammadalibutt30@gmail.com>
2025-03-07 10:49:31 +0500

    Re-evaulated map generation pipeline

---
+++ maps/generate_bindings.sh
@@ -0,0 +1,1 @@
+python3 ./generator.py ./ffi.map.gen ../mjui/fltk_bindings/
--- maps/generator.py
+++ maps/generator.py
@@ -2,6 +2,7 @@
 A tiny script that takes a .map.gen and converts it to ffi bindings
 """
 import json as j
+import copy
 import sys
 
 File = sys.argv[1] if sys.argv.__len__() > 0 else None
@@ -15,19 +16,26 @@ if File:
         raw_contents = f.read()
         try:
             parsed_contents: dict[str, dict] = j.loads(raw_contents)
-            print(parsed_contents)
         except:
             print('Error: Given file isn\'t a proper FFI map')
             sys.exit(1)
         
         definations = []
         bindings = []
-        
+        new_parsed_contents = copy.deepcopy(parsed_contents)
         for key, value in parsed_contents.items():
+            if key.startswith("@"):
+                if key == '@includes': 
+                    for file in value:
+                        with open(file) as f:
+                            parsed_import = j.loads(f.read())
+                            new_parsed_contents.update(parsed_import)
+
+        for key, value in new_parsed_contents.items():
             if key.startswith("@"):
                 continue
             if '@redef' in value.keys():
-                value = parsed_contents[value['@redef']]
+                value = new_parsed_contents[value['@redef']]
             args = [f"{arg_name}: {dtype}" for arg_name, dtype in value['arguments'].items()]
             args_str = ', '.join(args)
             defination = f'{key.upper()}_DEFINATION'
@@ -37,4 +45,4 @@ if File:
             definations.append(defination_alias)
             
         with open(OutputDir + 'bindings.mojo', 'w') as f:
-            f.write(f'"""Auto generated using generator.py"""\n\n\nfrom .dll import __dll\nfrom .function_definations import *\nfrom ..types import *\n\n{"\n".join(definations)}\n\n{"\n".join(bindings)}')
+            f.write(f'"""Auto generated using generator.py"""\n\n\nfrom .dll import __dll\nfrom ..types import *\n\n{"\n".join(definations)}\n\n{"\n".join(bindings)}')
